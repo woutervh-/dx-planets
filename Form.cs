7fc5e0c8ca0d3c476f0b9e662a3022973be6a793
Wouter van Heeswijk <woutervh@gmail.com>
2021-02-03 20:22:04 +0100

    Hello world with SharpDX and dotnet core on Windows

---
+++ Form.cs
@@ -0,0 +1,24 @@
+﻿namespace dx_planets
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        public Form(int width, int height)
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(width, height);
+            this.Text = "Form1";
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
